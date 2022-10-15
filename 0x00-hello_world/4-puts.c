a74261d7b38f815a14c7bffdf7895f46d9f70516
swoji <davidookuku@gmail.com>
2022-10-15 03:16:55 +0300

    Prints exact statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - this function prinst the string input
- *   * Description: the main function is used to print a string
- *    * Return: 0
- *     */
+ * main - this function prinst the string input
+ * Description: the main function is used to print a string
+ * Return: 0
+ * /
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
