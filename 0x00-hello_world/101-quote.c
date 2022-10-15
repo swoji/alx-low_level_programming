b053777d9a8cbb512d52eefacdf3354428fc6e1d
swoji <davidookuku@gmail.com>
2022-10-15 04:02:37 +0300

    Prints exactly to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - this function prints a string with write
+ * Description: the pogram prints a cool statement
+ * Return: 1
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n",59);
+return (1);
+}
