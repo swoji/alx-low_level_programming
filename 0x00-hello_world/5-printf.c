5a2b888e071b05e727bbced22aa16e0bc2993e05
swoji <davidookuku@gmail.com>
2022-10-15 03:41:09 +0300

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * main - this function prints a string with print f
  * Description: the rpogram prints a coll statement
  * Return: 0
- * /
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
