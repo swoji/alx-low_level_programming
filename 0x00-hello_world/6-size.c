59214d44e22547e12e791e043d83e615c5a9c136
swoji <davidookuku@gmail.com>
2022-10-15 03:49:07 +0300

    Add prints the size of various types on the computer it is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints size of various types
+ * Description: prints size of various types on the computer when compiled
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+char c;
+float f;
+long li;
+long long lli;
+
+printf("Size of a char: %ld byte(s)\n", sizeof(c));
+printf("Size of an int: %ld byte(s)\n", sizeof(i));
+printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+printf("Size of a float: %ld byte(s)\n", sizeof(f));
+return (0);
+}
