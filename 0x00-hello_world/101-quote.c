8215e2b36a55491df21d9a0cb873aa19a5f1c7ed
swoji <davidookuku@gmail.com>
2022-10-15 04:41:41 +0300

    Prints exactly to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - this function prints a string with write
- * Description: the pogram prints a cool statement
+ * main - Entry
  * Return: 1
  */
 int main(void)
 {
-    write(2, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n",59);
-    return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
