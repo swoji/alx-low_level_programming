2d6ffbc160d77bba6b84e1d58289575a6058fe65
swoji <davidookuku@gmail.com>
2022-10-19 00:07:32 +0300

    C function that prints the times table of an input value, starting with 0, if input greater than 15 or less than 0 function prints nothing

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,31 +10,35 @@ void print_times_table(int n)
 {
 	int num, mult, prod;
 
-	if (n <= 15 && n >= 0)
+	if (n >= 0 && n <=15)
 	{
 		for (num = 0; num <= n; num++)
 		{
-			prod = num * mult;
-			if (prod >= 100)
+			_putchar('0');
+			for (mult = 1; mult <= n;mult++)
 			{
-				_putchar((prod / 100) + '0');
-				_putchar(((prod / 10) % 10) + '0');
-			}
-			else if (prod < 100 && prod >= 10)
-			{
-				_putchar((prod / 10) + '0');
-			}
-			_putchar((prod % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
 
-			if (mult == n)
-				continue;
-			_putchar(',');
-			_putchar(' ');
-			if (prod < 100)
-				_putchar(' ');
-			if (prod < 10)
-				_putchar(' ');
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
