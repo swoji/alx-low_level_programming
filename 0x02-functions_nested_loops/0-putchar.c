ca50489f1f6981ddd0252965f3e6d29318839cb9
swoji <davidookuku@gmail.com>
2022-10-18 15:20:46 +0300

    C program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * prints _putchar, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        _putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ALX programme where I learned about nested loops, header files, and variable scope in C while practicing declaring and defining my own functions
