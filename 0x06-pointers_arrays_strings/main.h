edace44cda734b78410c7739f34edd084f388f37
swoji <davidookuku@gmail.com>
2022-10-27 14:48:18 +0300

    Header file containing prototypes for all functions written in the project.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 /**
  * File: main.h
  * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
+ *       used in the 0x06-pointers_arrays_strings directory.
  */
 
 int _putchar(char c);
