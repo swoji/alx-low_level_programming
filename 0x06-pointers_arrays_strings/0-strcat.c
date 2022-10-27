5e87f2fec5dba4284279e694aad10dda750ded51
swoji <davidookuku@gmail.com>
2022-10-27 22:17:39 +0300

    C function that concatenates two strings. Adds a terminating null byte at end.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,16 +12,16 @@ char *_strcat(char *dest, char *src)
 {
 	int s = 0, i = 0;
 
-	while (dest[s] != 0)
+	while (dest[s] != '\0')
 	{
 		s++;
 	}
-	while (src[i] != 0)
+	while (src[i] != '\0')
 	{
 		dest[s + i] = src[i];
 		i++;
 	}
 
-	dest[s + i] = 0;
+	dest[s + i] = '\0';
 	return (dest);
 }
