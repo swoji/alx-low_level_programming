0780997fb655bff695954282f8e85d2e3a7a7abc
swoji <davidookuku@gmail.com>
2022-10-28 17:34:14 +0300

    Prints the times table of the input, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,37 +8,43 @@
  */
 void print_times_table(int n)
 {
-	int num, mult, prod;
+	int a, b, c, d, e, f;
 
-	if (n >= 0 && n <= 15)
+	if (n >= 0 && n < 15)
 	{
-		for (num = 0; num <= n; num++)
+		for (a = 0; a <=  n; a++)
 		{
-			_putchar('0');
-			for (mult = 1; mult <= n; mult++)
+			for (b = 0; b <= n; b++)
 			{
-				_putchar(',');
-				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-
-				if (prod >= 100)
+				c = a * b; d = c / 100; e = (c /  10) % 10;
+				f = (c % 100) % 10;
+				if (b == 0)
 				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
+					_putchar('0');
 				}
-				else if (prod <= 99 && prod >= 10)
+				else if (c < 10)
 				{
-					_putchar((prod / 10) + '0');
+					_putchar(' '); _putchar(' ');
+					_putchar('0' + f);
 				}
-				_putchar((prod % 10) + '0');
+				else if (c < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + e); _putchar('0' + f);
+				}
+				else
+				{
+					_putchar('0' + d);
+					_putchar('0' + e);
+					_putchar('0' + f);
+				}
+				if (b < n)
+				{
+					_putchar(','); _putchar(' ');
+				}
+				else
+					_putchar('\n');
 			}
-			_putchar('\n');
 		}
 	}
 }
