9eb5481582560f9060d776194870e93f29fc49bb
swoji <davidookuku@gmail.com>
2022-11-01 15:22:18 +0300

    Header file containing prototypes for all functions written in the project

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 /**
  * File: main.h
  * Description: Header file containing prototypes for all functions
- *              used in the 0x06-pointers_arrays_strings directory.
+ *              used in the 0x07-pointers_arrays_strings directory.
  */
 
 #include <stddef.h>
