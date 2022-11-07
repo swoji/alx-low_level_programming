19307d2fbc25a2ec3e7eb06737d1d6bbcf54b75d
swoji <davidookuku@gmail.com>
2022-11-08 02:17:36 +0300

    C program that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: 0 if no errors, else 1
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, k;
+
+	if (argc != 3)
+	{
+		puts("Error");
+		return (1);
+	}
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	k = i * j;
+	printf("%d\n", k);
+	return (0);
+}
