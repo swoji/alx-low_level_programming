720a4dab6019a929c0de86b65905fa65a5b1c438
swoji <davidookuku@gmail.com>
2022-11-18 14:37:16 +0300

    Variadic function that prints numbers, followed by a new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,9 +5,10 @@
 /**
  * print_numbers - prints numbers, followed by a new line
  * @separator: string to be printed between numbers
- * @n - number of integers passed to the function
+ * @n: number of integers passed to the function
+ * 
  * Return: void
-*/
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
     unsigned int i;
