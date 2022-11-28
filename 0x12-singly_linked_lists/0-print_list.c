aada8d279f7d70730db9bb4242cfbd383df3c901
swoji <davidookuku@gmail.com>
2022-11-28 14:12:25 +0300

    prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
+
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the start of the list
  * 
  * Return: number of nodes in the list
  */
-
 size_t print_list(const list_t *h)
 {
 	const list_t *tmp;
